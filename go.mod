386428119b08d8bedb399374254fc20cdaa32e54
Matt Finger <matthewfinger3@gmail.com>
2022-12-31 19:41:12 -0600

    Recursive looping

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/matthewfinger/golang_tutorial.git
 
 go 1.19
 
-require github.com/matthewfinger/golang_greetings v0.1.0-alpha
+require github.com/matthewfinger/golang_greetings v1.0.0
--- hello.go
+++ hello.go
@@ -4,18 +4,30 @@ import (
 	"fmt"
 	"github.com/matthewfinger/golang_greetings"
 	"log"
+	"strings"
 )
+
+func NameLoop() {
+	var name string
+	fmt.Print("Enter a name: ")
+	fmt.Scanf("%s", &name)
+	if (strings.ToLower(name) == "exit") {
+		fmt.Println("Bye bye")
+		return
+	}
+	message, err := golang_greetings.Hello(name)
+	if (err != nil) {
+		fmt.Println("You entered nothing or something stupid")
+		NameLoop()
+	}
+	fmt.Printf("%v\n", message)
+	if (name != "exit") {
+		NameLoop()
+	}
+}
 
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	var name string
-	fmt.Print("Enter a name: ")
-	fmt.Scanf("%s", &name)
-	message, err := golang_greetings.Hello(name)
-	if (err != nil) {
-		log.Fatal(err)
-	}
-	fmt.Printf("%#v\n", message)
-	fmt.Println(golang_greetings.Greet("Evan", false))
+	NameLoop()
 }
