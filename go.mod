64022888a3242be7fc22dda32348de03fb63a36f
Matt Finger <matthewfinger3@gmail.com>
2023-01-01 11:40:52 -0600

    Added the ability to use multiple names in one input

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/matthewfinger/golang_tutorial.git
 
 go 1.19
 
-require github.com/matthewfinger/golang_greetings v1.0.0
+require github.com/matthewfinger/golang_greetings v1.0.1
--- hello.go
+++ hello.go
@@ -5,23 +5,48 @@ import (
 	"github.com/matthewfinger/golang_greetings"
 	"log"
 	"strings"
+	"bufio"
+	"os"
 )
 
+
 func NameLoop() {
-	var name string
-	fmt.Print("Enter a name: ")
-	fmt.Scanf("%s", &name)
-	if (strings.ToLower(name) == "exit") {
+	var input string
+	fmt.Print("Enter a name (or multiple separated by commas): ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		fmt.Println("You entered nothing or something stupid")
+		NameLoop()
+		return
+	}
+
+	input = scanner.Text()
+
+	if (strings.ToLower(input) == "exit") {
 		fmt.Println("Bye bye")
 		return
 	}
-	message, err := golang_greetings.Hello(name)
+	components := strings.Split(input, ",")
+	names := make([]string, 0, len(components))
+	for _, component := range components {
+		cleaned := strings.TrimSpace(component)
+		if (cleaned != "") {
+			names = append(names, cleaned)
+		}
+	}
+
+	messages, err := golang_greetings.Hellos(names)
 	if (err != nil) {
 		fmt.Println("You entered nothing or something stupid")
 		NameLoop()
+		return
 	}
-	fmt.Printf("%v\n", message)
-	if (name != "exit") {
+	for _, message := range messages {
+		fmt.Printf("%v\n", message)
+	}
+	if (input != "exit") {
 		NameLoop()
 	}
 }
